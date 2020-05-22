b7301085b6bfd36d85edd6d4f732b720eac7f7f6
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-05-22 18:29:48 -0300

    logic done

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const X_CLASS = "x"
 const CIRCLE_CLASS = "circle"
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+    [6, 4, 2],
+    [0, 4, 8]
 
+]
 const cellElements = document.querySelectorAll("[data-cell]")
 const board = document.getElementById("board")
-let circleTurn = ""
+const winningMessage = document.querySelector(".winning-message")
+const winningMessageText = document.querySelector("[data-winning-message-text]")
+const restartBtn = document.getElementById("restart-btn")
+let circleTurn = null
+
 
 
 const placeMark = (cell, currentClass) => {
@@ -26,21 +40,65 @@ const setBoardHoverClass = () => {
 
 }
 
+const checkwin = (currentClass) => {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+}
+
+
+const isDraw = () => {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+
+    })
+}
+
 const handleClick = (e) => {
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
 
-    swapTurns()
-    setBoardHoverClass()
+    if (checkwin(currentClass)) {
+        endGame(false)
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurns()
+        setBoardHoverClass()
+    }
 }
 
 
 const startGame = () => {
+    circleTurn = false
     cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
         cell.addEventListener("click", handleClick, {
             once: true
         })
     })
+    setBoardHoverClass()
+    winningMessage.classList.remove("show")
+}
+
+startGame()
+
+
+
+const endGame = (draw) => {
+    if (draw) {
+        winningMessageText.innerText = `draw!`
+    } else {
+        winningMessageText.innerText = `${circleTurn ? "O" : "X"} Wins!`
+    }
+
+    winningMessage.classList.add("show")
 
 }
+
+
+restartBtn.addEventListener("click",startGame)
